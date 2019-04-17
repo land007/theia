51ab361c1002e6215ad494932655e9a5278b6436
Vincent Fugnitto <vincent.fugnitto@ericsson.com>
2019-04-17 11:33:44 -0400

    Add support for recently used commands
    Fixes #2518
    
    Added support for recently used commands.
    Commands which have been recently executed will now be placed above others
    when triggering `quick-commands` making it easier for users to see their
    favorite or last recently used commands.
    
    Signed-off-by: Vincent Fugnitto <vincent.fugnitto@ericsson.com>

--- packages/core/src/browser/quick-open/quick-command-service.ts
+++ packages/core/src/browser/quick-open/quick-command-service.ts
@@ -17,7 +17,7 @@
 import { inject, injectable } from 'inversify';
 import { Command, CommandRegistry } from '../../common';
 import { Keybinding, KeybindingRegistry } from '../keybinding';
-import { QuickOpenModel, QuickOpenItem, QuickOpenMode } from './quick-open-model';
+import { QuickOpenModel, QuickOpenItem, QuickOpenMode, QuickOpenGroupItem, QuickOpenGroupItemOptions } from './quick-open-model';
 import { QuickOpenOptions } from './quick-open-service';
 import { QuickOpenContribution, QuickOpenHandlerRegistry, QuickOpenHandler } from './prefix-quick-open-service';
 import { ContextKeyService } from '../context-key-service';
@@ -32,10 +32,10 @@ export class QuickCommandService implements QuickOpenModel, QuickOpenHandler {
     readonly description: string = 'Quick Command';
 
     @inject(CommandRegistry)
-    protected readonly commands: CommandRegistry;
+    protected readonly commandRegistry: CommandRegistry;
 
     @inject(KeybindingRegistry)
-    protected readonly keybindings: KeybindingRegistry;
+    protected readonly keybindingRegistry: KeybindingRegistry;
 
     @inject(ContextKeyService)
     protected readonly contextKeyService: ContextKeyService;
@@ -51,15 +51,9 @@ export class QuickCommandService implements QuickOpenModel, QuickOpenHandler {
     init(): void {
         // let's compute the items here to do it in the context of the currently activeElement
         this.items = [];
-        const filteredAndSortedCommands = this.commands.commands.filter(a => a.label).sort((a, b) => Command.compareCommands(a, b));
-        for (const command of filteredAndSortedCommands) {
-            if (command.label) {
-                const contexts = this.contexts.get(command.id);
-                if (!contexts || contexts.some(when => this.contextKeyService.match(when))) {
-                    this.items.push(new CommandQuickOpenItem(command, this.commands, this.keybindings));
-                }
-            }
-        }
+        const valid = this.getValidCommands(this.commandRegistry.commands);
+        const { recent, other } = this.getCommands(this.commandRegistry.recent, valid);
+        this.items.push(...this.buildRecentItems(recent), ...this.buildOtherItems(other, recent.length));
     }
 
     public onType(lookFor: string, acceptor: (items: QuickOpenItem[]) => void): void {
@@ -74,9 +68,111 @@ export class QuickCommandService implements QuickOpenModel, QuickOpenHandler {
         return { fuzzyMatchLabel: true };
     }
 
+    /**
+     * Builds the list of recently used `CommandQuickOpenItem`.
+     *
+     * @param recent the list of recently used commands.
+     * @returns the list of recently used `CommandQuickOpenItem`.
+     */
+    private buildRecentItems(recent: Command[]): CommandQuickOpenItem[] {
+        const items: CommandQuickOpenItem[] = [];
+        recent.forEach((command, index) => {
+            items.push(new CommandQuickOpenItem(
+                command,
+                this.commandRegistry,
+                this.keybindingRegistry,
+                {
+                    groupLabel: index === 0 ? 'recently used' : '',
+                    showBorder: false,
+                }
+            ));
+        });
+        return items;
+    }
+
+    /**
+     * Builds the list of other `CommandQuickOpenItem`.
+     *
+     * @param other the list of other commands.
+     * @param recentLength the length of the recently used commands list.
+     * @returns the list of recently used `CommandQuickOpenItem`.
+     */
+    private buildOtherItems(other: Command[], recentLength: number): CommandQuickOpenItem[] {
+        const items: CommandQuickOpenItem[] = [];
+        other.forEach((command, index) => {
+            items.push(new CommandQuickOpenItem(
+                command,
+                this.commandRegistry,
+                this.keybindingRegistry,
+                {
+                    groupLabel: recentLength <= 0 ? '' : index === 0 ? 'other commands' : '',
+                    showBorder: recentLength <= 0 ? false : index === 0 ? true : false,
+                }
+            ));
+        });
+        return items;
+    }
+
+    /**
+     * Get the list of recently used and other commands.
+     *
+     * @param recentCommands the list of recent commands.
+     * @param allCommands the list of all commands.
+     * @returns the list of recently used commands and other commands.
+     */
+    private getCommands(recentCommands: Command[], allCommands: Command[]): { recent: Command[], other: Command[] } {
+
+        const recent: Command[] = [];
+        recentCommands.forEach((r: Command) => {
+            const exists = [...allCommands].some((c: Command) => Command.equals(r, c));
+            // Add the recently used item to the list.
+            if (exists) { recent.push(r); }
+        });
+
+        const other: Command[] = [];
+        allCommands.forEach((a: Command) => {
+            const exists = [...recent].some((c: Command) => Command.equals(a, c));
+            // If the command does not exist in the recently used list, add it to the other list.
+            if (!exists) { other.push(a); }
+        });
+
+        return { recent, other };
+    }
+
+    /**
+     * Filter commands which do not have a label, and sort them alphabetically.
+     *
+     * @param commands the list of raw commands.
+     * @returns the list of filtered, and sorted commands.
+     */
+    private sortAndFilter(commands: Command[]): Command[] {
+        return commands
+            .filter((a: Command) => a.label)
+            .sort((a: Command, b: Command) => Command.compareCommands(a, b));
+    }
+
+    /**
+     * Get the list of valid commands.
+     *
+     * @param commands the list of raw commands.
+     * @returns the list of valid commands.
+     */
+    private getValidCommands(raw: Command[]): Command[] {
+        const valid: Command[] = [];
+        raw.forEach((command: Command) => {
+            if (command.label) {
+                const contexts = this.contexts.get(command.id);
+                if (!contexts || contexts.some(when => this.contextKeyService.match(when))) {
+                    valid.push(command);
+                }
+            }
+        });
+        return this.sortAndFilter(valid);
+    }
+
 }
 
-export class CommandQuickOpenItem extends QuickOpenItem {
+export class CommandQuickOpenItem extends QuickOpenGroupItem {
 
     private activeElement: HTMLElement;
     private hidden: boolean;
@@ -84,9 +180,10 @@ export class CommandQuickOpenItem extends QuickOpenItem {
     constructor(
         protected readonly command: Command,
         protected readonly commands: CommandRegistry,
-        protected readonly keybindings: KeybindingRegistry
+        protected readonly keybindings: KeybindingRegistry,
+        protected readonly options: QuickOpenGroupItemOptions,
     ) {
-        super();
+        super(options);
         this.activeElement = window.document.activeElement as HTMLElement;
         this.hidden = !this.commands.getActiveHandler(this.command.id);
     }
